137110d9e417efc473ba3f2003e0b12edd60d1b2
unt32 <vladsaen5@gmail.com>
2024-10-23 20:01:39 +0300

    10

---
+++ home-manager/enviroment/hypr/hyprpaper.nix
@@ -0,0 +1,9 @@
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = "/home/unt32/nix/.icons/wallpaper.jpg";
+      wallpaper = ",/home/unt32/nix/.icons/wallpaper.jpg";
+    };
+  };
+}
