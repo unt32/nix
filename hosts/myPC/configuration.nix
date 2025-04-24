8661cfdee0a6888203f59a2fe4fd15a77ac3e820
unt32 <vladsaen5@gmail.com>
2025-04-24 22:18:11 +0300

    added a2dp; cleared myPC cofiguration; fixed black screen after suspend

--- hosts/myPC/configuration.nix
+++ hosts/myPC/configuration.nix
@@ -19,13 +19,6 @@
   };
 
 
-  services.dwm-status = {
-      enable = true;
-      extraConfig = builtins.readFile ./dwm-status.toml;
-      order = [ "audio" "network" "time" ];
-  };
-
-
   hardware = {
     bluetooth = {
           enable = true;
@@ -39,79 +32,83 @@
             JustWorksRepairing = "always";
             Class = "0x000100";
             FastConnectable = true;
+            Enable = "Source,Sink,Media,Socket";
           };
     };
+
     xpadneo.enable = true;
+
+    nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = true;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+    };
+
+    graphics = {
+      enable = true;
+    };
+
   };
 
   security.rtkit.enable = true;
-  services.pipewire = {
-          enable = true;
-          alsa.enable = true;
-          alsa.support32Bit = true;
-          pulse.enable = true;
-          # If you want to use JACK applications, uncomment this
-          #jack.enable = true;
-  };
-
   services = {
-    blueman.enable = true;
-    pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-            "monitor.bluez.properties" = {
-                "bluez5.enable-sbc-xq" = true;
-                "bluez5.enable-msbc" = true;
-                "bluez5.enable-hw-volume" = true;
-                "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    dwm-status = {
+        enable = true;
+        extraConfig = builtins.readFile ./dwm-status.toml;
+        order = [ "audio" "network" "time" ];
+    };
+
+    pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+            # If you want to use JACK applications, uncomment this
+            #jack.enable = true;
+            wireplumber = {
+              configPackages = [
+                (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/11-bluetooth-policy.conf" ''
+                  wireplumber.settings = { bluetooth.autoswitch-to-headset-profile = false }
+                '')
+              ];
             };
     };
-  };
 
+    blueman.enable = true;
+    xserver.videoDrivers = ["nvidia"];
+  };
 
   programs.steam = {
           enable = true;
           gamescopeSession.enable = true;
-          remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-          dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+          remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+          dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
           localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  #services.openssh.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
 }
